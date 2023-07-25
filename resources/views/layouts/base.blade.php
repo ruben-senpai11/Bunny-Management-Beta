6228b38d78bee9f5b10c8a5f31c179e7fdaa43ea
gohouerodias <gohouerodias@gmail.com>
2023-07-25 20:07:14 +0100

    oki

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -31,7 +31,11 @@
 
     <!-- Volt CSS -->
     <link type="text/css" href="   {{ asset('css/volt.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/searchpanes/1.3.0/css/searchPanes.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.3/css/select.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css" />
     <!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
     @yield("style")
 
@@ -132,8 +136,17 @@
     <!-- Volt JS -->
     <script src="../../assets/js/volt.js"></script>
 
+    <!--datatales cdn implementation-->
     <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
-    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/searchpanes/1.3.0/js/dataTables.searchPanes.min.js"></script>
+    <script src="https://cdn.datatables.net/select/1.3.3/js/dataTables.select.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
 
     <script>
         @if (Session::get('error'))
--- resources/views/pages/bunny-list.blade.php
+++ resources/views/pages/bunny-list.blade.php
@@ -1,55 +1,96 @@
 @extends('layouts.base')
 
 @section('content')
-  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
-   <a  href="{{ route('create-bunny', []) }}"
-    class="btn btn-outline-gray-500  dashed-outline new-card" ><svg class="icon icon-xs me-2" fill="none"
-      stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+  <a href="{{ route('create-bunny', []) }}" class="btn btn-outline-gray-500  dashed-outline new-card"><svg
+      class="icon icon-xs me-2" fill="none" stroke="currentColor" viewbox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
       </path>
     </svg>Record new Rabbit</a>
-    
-   
-  </div>
-  <!--Start code list product-->
- <div class="card p-2">
-    <table id="bunnyTable" class="display">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Uid</th>
-            <th>Gender</th>
-            <th>Destination</th>
-            <th>Date birth</th>
-          </tr>
-        </thead>
-        <tbody>
-      
-        </tbody>
-      </table>
-    
-  </div>
-  <!--End code list product-->
-  
- 
+
+
+</div>
+<!--Start code list product-->
+<div class="card p-2">
+  <table id="bunnyTable" class="hover" style="width:100%">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Uid</th>
+        <th>Gender</th>
+        <th>Destination</th>
+        <th>Date birth</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+   <tfoot>
+     <tr>
+        <th>Id</th>
+        <th>Uid</th>
+        <th>Gender</th>
+        <th>Destination</th>
+        <th>Date birth</th>
+        <th>Action</th>
+      </tr>
+    </tfoot>
+  </table>
+
+</div>
+<!--End code list product-->
+
+
 
 @endsection
 @section('script')
 <script>
   let url = "{{route('get-list-bunny')}}"
-      $('#bunnyTable').DataTable( {
-        ajax: {
-          url: url,
-          dataSrc: 'bunnies'
-        },
-        processing: true,
-        columns: [
-          { data: 'id' },
-          { data: 'Uid' },
-          { data:'gender' },
-          { data: 'destination' },
-          { data: 'date_birth' },
-        ]
-      } );
+
+  
+
+      var table = new DataTable('#bunnyTable', {
+          responsive: true,
+          dom: 'Bfrtip',
+          buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print',{
+              extend: 'searchPanes',
+              config: {
+                cascadePanes: true
+              }
+            }
+          ],
+          ajax: {
+            url: url,
+            dataSrc: 'bunnies'
+          },
+          processing: true,
+          columns: [
+            { data: 'id' },
+            { data: 'Uid' },
+            { data:'gender' },
+            { data: 'destination' },
+            { 
+              data: 'date_birth',
+              visible: false
+            },
+            {
+              data: null,
+              defaultContent: '<button class="btn btn-primary " >Click!</button>',
+              targets: -1
+            }
+          
+          ],columnDefs: [
+            {
+              targets: 4,
+              render: DataTable.render.date('D MMM YYYY')
+            },
+          ]
+      });
+
+      table.on('click', 'button', function (e) {
+        let data = table.row(e.target.closest('tr')).data();
+        console.log(data);
+        alert(data[0] + "'s salary is: " + data["Uid"]);
+      });
 </script>
 @endsection
