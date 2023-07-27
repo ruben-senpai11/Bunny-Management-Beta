9321cfcc7927ee5f903e8f20c2ce2c5907b1cc11
gohouerodias <gohouerodias@gmail.com>
2023-07-27 08:39:00 +0100

    correction transaction

--- resources/views/sections/left-sidebar.blade.php
+++ resources/views/sections/left-sidebar.blade.php
@@ -186,26 +186,34 @@
                     </ul>
                 </div>
             </li>
-            <li class="nav-item"><span class="nav-link collapsed d-flex justify-content-between align-items-center"
-                    data-bs-toggle="collapse" data-bs-target="#submenu-transactions"><span><span
-                            class="sidebar-icon"><svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
-                                <path fill-rule="evenodd"
-                                    d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-                                    clip-rule="evenodd"></path>
-                            </svg></span><span class="sidebar-text">Transactions</span> </span><span
-                        class="link-arrow"><svg class="icon icon-sm" fill="currentColor" viewbox="0 0 20 20"
+        <li class="nav-item">
+            <span class="nav-link collapsed d-flex justify-content-between align-items-center"
+                data-bs-toggle="collapse" data-bs-target="#submenu-transactions">
+                <span>
+                    <span
+                        class="sidebar-icon">
+                        <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
+                            <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
                             <path fill-rule="evenodd"
-                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
                                 clip-rule="evenodd"></path>
-                        </svg></span><span class="sidebar-text">Transactions</span> </span><span class="link-arrow"><svg
-                        class="icon icon-sm" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clip-rule="evenodd"></path>
-                    </svg></span></span>
+                        </svg>
+                    </span>
+                    <span class="sidebar-text">Transactions</span> 
+                </span>
+               
+                <span class="link-arrow">
+                    <svg
+                    class="icon icon-sm" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </span>
+        </span>
+            
+            
             <div class="multi-level collapse" role="list" id="submenu-transactions" aria-expanded="false">
                 <ul class="flex-column nav">
                     <li class="nav-item"><a class="nav-link" target="_blank"
@@ -236,9 +244,13 @@
                     class="sidebar-text">Paramètres</span></a></li>
         <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
         
-        <li class="nav-item"><a href="https://themesberg.com/" target="_blank" class="nav-link d-flex align-items-center"><span
+        <li class="nav-item"><a href="{{ route('logout') }}" 
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+            target="_blank" class="nav-link d-flex align-items-center"><span
                     class="sidebar-icon"><img src="{{ asset('assets/img/logout.svg') }}" height="20" width="28" alt="Logout">
                 </span><span class="sidebar-text">Se déconnecter</span></a></li>
+
+               
         </ul>
     </div>
 </nav>
